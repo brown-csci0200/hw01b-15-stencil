63b35c1cc9612d4de18f703064871a3879f4030b
tz-499 <kevin_t_zhu@brown.edu>
2025-02-01 16:28:47 -0500

    Changed stencil such that auatograder doesn't throw java syntax mass error

--- sol/ListUtils.java
+++ sol/ListUtils.java
@@ -1,31 +1,46 @@
 package sol;
 
+import src.FuncList;
+import src.IFuncList;
+
 /**
  * Utility Functions for IFuncList
  */
 public class ListUtils {
-
+    // Trick: Use Cmd + / OR Ctrl + / to (un)comment highlighted line
     // TODO: write Javadoc based on description from assignment handout
-    // TODO: uncomment and implement
-    // public static boolean member(IFuncList list, Object item) { }
+     public static boolean member(IFuncList list, Object item) {
+         // TODO: replace with your implementation
+         return false;
+     }
 
-    // TODO: write Javadoc based on description from assignment handout
-    // TODO: uncomment and implement
-    // public static IFuncList<Integer> insert(IFuncList<Integer> list, int item) { }
+    // TODO: write Javadocs
+     public static IFuncList<Integer> insert(IFuncList<Integer> list, int item) {
+         // TODO: replace with your implementation
+         return new FuncList<Integer>();
+     }
 
-    // TODO: write Javadoc based on description from assignment handout
-    // TODO: uncomment and implement
-    // public static IFuncList<String> spellCheck(IFuncList<String> list) { }
+    // TODO: write Javadocs
+     public static IFuncList<String> spellCheck(IFuncList<String> list) {
+         // TODO: replace with your implementation
+         return new FuncList<String>();
+     }
 
-    // TODO: write Javadoc based on description from assignment handout
-    // TODO: uncomment and implement
-    // public static boolean allInRange(IFuncList<Integer> numList, int low, int high) { }
+    // TODO: write Javadocs
+     public static boolean allInRange(IFuncList<Integer> numList, int low, int high) {
+         // TODO: replace with your implementation
+         return false;
+     }
 
-    // TODO: write Javadoc based on description from assignment handout
-    // TODO: uncomment and implement
-    // public static int daysInRange(IFuncList<IFuncList<Integer>> dayList, int low, int high) { }
+    // TODO: write Javadocs
+     public static int daysInRange(IFuncList<IFuncList<Integer>> dayList, int low, int high) {
+         // TODO: replace with your implementation
+         return 0;
+     }
 
-    // TODO: write Javadoc based on description from assignment handout
-    // TODO: uncomment and implement
-    // public static FuncList<Integer> quicksort(FuncList<Integer> numList) { }
+    // TODO: write Javadocs
+     public static FuncList<Integer> quicksort(FuncList<Integer> numList) {
+         // TODO: replace with your implementation
+         return new FuncList<Integer>();
+     }
 }
